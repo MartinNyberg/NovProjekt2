3477570cf4d732f071d5979d85ca06d6737a889b
MartinNyberg <55439200+MartinNyberg@users.noreply.github.com>
2019-11-18 11:05:05 +0100

    cumit 1-500

---
+++ Api övning/Api övning/Pokemon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api_övning
+{
+    class Pokemon
+    {
+        public string name;
+
+        public int base_experience;
+
+        public string version;
+
+    }
+}
---
+++ Api övning/Api övning/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+
+
+namespace Api_övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RestClient client = new RestClient("https://pokeapi.co/api/v2/");
+
+            RestRequest request = new RestRequest("pokemon/electabuzz/");
+
+            IRestResponse response = client.Get(request);
+
+            Pokemon magmar = JsonConvert.DeserializeObject<Pokemon>(response.Content);
+
+            //Console.WriteLine(response.Content);
+
+            Console.WriteLine(magmar.name + "[" + magmar.base_experience + "]");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
